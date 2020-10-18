558cf0cb1045bc478e7964d95ff0129ad31c8c6a
hieunguyentrong1996 <73066578+hieunguyentrong1996@users.noreply.github.com>
2020-10-18 20:59:33 +0700

    Add project files.

---
+++ Log4Net/Program.cs
@@ -0,0 +1,55 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    class Program
+    {
+        // Declare an instance for log4net
+        private static readonly ILog Log4Net = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            ImplementLoggingFunction();
+        }
+
+        private static void ImplementLoggingFunction()
+        {
+            /*
+             FATAL
+             ERROR
+             WARN
+             INFO
+             DEBUG
+             */
+            var secs = 3;
+            Log4Net.Fatal("Start log Fatal");
+            Console.WriteLine("Start log Fatal");
+
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep 3 secs
+            Log4Net.Error("Start log Error...");
+            Console.WriteLine("Start log Error...");
+
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep 3 secs
+            Log4Net.Warn("Start log Warn...");
+            Console.WriteLine("Start log Warn...");
+
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep 3 secs
+            Log4Net.Info("Start log Info...");
+            Console.WriteLine("Start log Info...");
+
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep 3 secs
+            Log4Net.Debug("Start log Debug...");
+            Console.WriteLine("Start log Debug...");
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Log4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Log4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Log4Net")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Let: log4net know that it can look for configation in the default application config file
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("362d41a1-829e-464b-a1ad-ae35c26ab281")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
